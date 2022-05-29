3ec310a2766fd243cf5da07ae4df2675050a4066
DefrinDan <dan.defrin@memicmed.com>
2022-05-29 10:23:45 +0300

    First commit

---
+++ DevOpsExpirements/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevOpsExpirements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ValueGen calc = new ValueGen();
+        }
+    }
+}
---
+++ DevOpsExpirements/ValueGen.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevOpsExpirements
+{
+    internal class ValueGen : ICalc
+    {
+        Random rnd = new Random();
+        int x, y, result;
+
+        //ctor
+        public ValueGen()
+        {
+            InitValues();
+        }
+
+        //Init value
+        private void InitValues()
+        {
+            this.x = rnd.Next();
+            this.y = rnd.Next();
+        }
+
+        public string Add(int x, int y)
+        {
+            result = x + y;
+            return $"{x} + {y} = {result}";
+        }
+
+        public string Div(int x, int y)
+        {
+            result = x / y;
+            return $"{x} / {y} = {result}";
+        }
+
+        public string Mult(int x, int y)
+        {
+            result = x * y;
+            return $"{x} x {y} = {result}";
+        }
+
+        public string Sub(int x, int y)
+        {
+            result = x - y;
+            return $"{x} - {y} = {result}";
+        }
+
+
+
+        
+    }
+}
